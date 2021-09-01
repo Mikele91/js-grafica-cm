56ad3c15c53df138b2a52c97826bbf4795b99f30
Mikele91 <michele.amoruso91.8@gmail.com>
2021-09-01 17:18:52 +0200

    terimanto

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
-// Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
-let numero = parseInt(prompt("inserisci un numero multiplo di 10 ")) ;
-while( ((numero % 10)!=0) ){
-        numero =  parseInt(prompt(" ATTENZIONE inserisci un numero multiplo di 10 "));
+function CreaQuadrato(numero){
+    for ( let i= 1; i <= numero; i++){
+        document.getElementById("campo").innerHTML+=`<div class="quadrato">${i}</div>`;
+    }
+
 }
 
-console.log(numero);
-// Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
-for ( let i= 1; i <= numero; i++){
-    document.getElementById("campo").innerHTML+=`<div class="quadrato">${i}</div>`;
+
+
+// Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
+let numeroUtente = parseInt(prompt("inserisci un numero multiplo di 10 ")) ;
+while( ((numeroUtente % 10)!=0) ){
+        numero =  parseInt(prompt(" ATTENZIONE inserisci un numero multiplo di 10 "));
 }
-document.addEventListener("click",
+CreaQuadrato(numeroUtente);
+
+
+// Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
+
+document.getElementById("campo").addEventListener("click",
 function(event){
     event.target.classList.toggle("color");
     alert(event.target.innerHTML)
+    console.log(event.target);
 }
 )
 
